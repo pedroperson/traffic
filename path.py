10f641ee26cba19af614867df2f2bf2f18c266d5
pedroperson <pedro.bom7@gmail.com>
2024-01-14 18:08:16 -0500

    cleaned up path

--- path.py
+++ path.py
@@ -1,5 +1,5 @@
-from random import choice, sample
-from typing import List, Optional, Tuple
+from random import choice
+from typing import List, Tuple
 
 from model import Direction, direction_deltas
 
@@ -8,12 +8,12 @@ IndexPoint = Tuple[int, int]
 
 class Path:
     def __init__(self, start: IndexPoint, end: IndexPoint):
-        self.s = start
-        self.e = end
+        # Keep an index to know where we are in the path
         self.index = 0
-
         pts, dirs = generate_paths_to_node(start, end)
+        # Keep a list of points to help find intersections
         self.path = pts
+        # Keep a list of directions to help with repositioning
         self.path_directions = dirs
 
     # Call this to move to the next target
@@ -21,30 +21,8 @@ class Path:
         if not self.reached_end():
             self.index += 1
 
-    def start(self):
-        return self.s
-
-    def end(self):
-        return self.e
-
     def reached_end(self):
         return self.index + 1 >= len(self.path)
-
-    def previous_target(self):
-        if self.index - 1 < 0:
-            return None
-        return self.path[self.index - 1]
-
-    def previous_direction(self):
-        if self.index - 1 < 0:
-            return None
-        return self.path_directions[self.index - 1]
-
-    def target(self):
-        return self.path[self.index]
-
-    def target_direction(self):
-        return self.path_directions[self.index]
 
     def next_target(self):
         if self.index + 1 >= len(self.path):
@@ -60,15 +38,17 @@ class Path:
 def generate_paths_to_node(
     start: IndexPoint, end: IndexPoint
 ) -> Tuple[List[IndexPoint], List[Direction]]:
-    pos: IndexPoint = tuple(start)
-    path_directions: List[Direction] = []
-    path_points: List[IndexPoint] = []
+    pos = tuple(start)
+    path_directions = []
+    path_points = []
     while pos != end:
         # For any two points, there are at most 2 directions we can go in if we want to get closer to the end point at every step
         dir = choice(possible_directions(pos, end))
-        path_directions.append(dir)
 
-        pos = add_tuples(pos, direction_deltas[dir])
+        delta = direction_deltas[dir]
+        pos = pos[0] + delta[0], pos[1] + delta[1]
+
+        path_directions.append(dir)
         path_points.append(pos)
 
     return path_points, path_directions
@@ -86,7 +66,3 @@ def possible_directions(start: IndexPoint, end: IndexPoint) -> List[Direction]:
         possible_directions.append(Direction.S)
 
     return possible_directions
-
-
-def add_tuples(x, y):
-    return (x[0] + y[0], x[1] + y[1])
